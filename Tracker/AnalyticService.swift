88bfed8fc3e75129b3bfad752663a349af582986
kira <pahareva.kira@ya.ru>
2023-12-04 23:55:57 +0300

    enh: sprint 17: localization, dark theme, snapshot tests, statisatics

---
+++ Tracker/AnalyticService.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticService.swift
+//  Tracker
+//
+//  Created by Кира on 04.12.2023.
+//
+
+import Foundation
---
+++ Tracker/FiltersViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FiltersViewController.swift
+//  Tracker
+//
+//  Created by Кира on 04.12.2023.
+//
+
+import Foundation
